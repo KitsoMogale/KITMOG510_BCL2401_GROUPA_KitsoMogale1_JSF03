8ee3015219cf688a682b41cfba963f2b9fada229
Kitso Mogale <kitsomogale19@gmail.com>
2024-08-07 09:33:58 +0200

    Filter component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 
 <template>
   <Header></Header>
+  <Filter/>
 
 </template>
 
 <script >
 
 import Header  from "./components/Header.vue";
+import Filter from "./components/Filter.vue"
 
 export default {
   name: 'App',
 
   components:{
-    Header
+    Header,
+    Filter
   }
 
 }
