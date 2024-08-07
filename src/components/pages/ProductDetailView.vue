dd44db126eb7f10641f85e280071e890fdeddbe6
Kitso Mogale <kitsomogale19@gmail.com>
2024-08-07 20:47:46 +0200

    fetchSingleProduct function in productdetailview

--- src/components/pages/ProductDetailView.vue
+++ src/components/pages/ProductDetailView.vue
@@ -1,16 +1,36 @@
 <template >
 
+<div class="flex justify-center">
+       
+      </div>
 
        <div class="flex justify-center">
-      {loading && !error ? <ProductSkeleton /> : <ProductDetail {...product} />}
-      ;
+       <ProductSkeleton v-if='loading && !error' />  
+       <ProductDetail v-if='!loading && !error' :product='product' />
+      
     </div>
 
 </template>
 
 
 <script setup>
-   import ProductSkeleton from '../products/ProductDetails.vue'
-   
+   import ProductSkeleton from '../products/ProductSkeleton.vue'
+   import ProductDetail from '../products/ProductDetail.vue'
+   import { fetchSingleProduct } from "../../api.js";
+   import {ref} from 'vue'
 
+   const loading = ref(false);
+   const error = ref(null);
+
+   const fetchData = async () => {
+      loading = true;
+      const { response, error } = await fetchSingleProduct(id);
+      if (error) {
+        setError(error);
+      }
+      setProduct(response);
+      loading = false;
+    };
+
+    fetchData();
 </script>
